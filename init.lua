5a6064cf3c65a7162ae46bac5d5214fb909c879c
hangj <guijie.han@gmail.com>
2025-05-16 18:14:31 +0800

    init config for neovide

--- init.lua
+++ init.lua
@@ -1,2 +1,26 @@
+local set = vim.keymap.set
+
+local log = io.open(vim.fs.abspath("~/dev/tmp/nvim-keymap.log"), "w+")
+if log then
+  vim.keymap.set = function(mode, key, action, opts, ...)
+    --    if key == "<leader>ff" then
+    --      log:write(debug.traceback())
+    --      log:write("\n")
+    --    end
+    log:write(string.format("%q, %q, %q, %q\n", mode, key, action, opts))
+    return set(mode, key, action, opts, ...)
+  end
+
+  -- log:write("api_info:\n")
+  -- log:write(vim.print(vim.fn.api_info()))
+end
+
+-- line number
+-- vim.fn.line('.')
+-- vim.fn.col('.')
+
+-- local cur_file_abs_path = vim.fn.expand("%:p")
+-- vim.notify('hello world')
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,39 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+if vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("i", "<D-s>", "<CMD>w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+
+-- command-left/right goto start/end of current line
+vim.keymap.set("i", "<D-Right>", "<ESC>A")
+vim.keymap.set("i", "<D-Left>", "<ESC>I")
+vim.keymap.set("n", "<D-Right>", "$")
+vim.keymap.set("n", "<D-Left>", "^")
+
+-- command-w close current buffer
+vim.keymap.set({ "i", "n", "v" }, "<D-w>", "<CMD>bd<CR>")
+
+-- Ctrl-Tab goto next buffer
+vim.keymap.set({ "i", "n", "v" }, "<C-Tab>", "<CMD>bnext<CR>")
+vim.keymap.set({ "i", "n", "v" }, "<C-S-Tab>", "<CMD>bprevious<CR>")
+
+-- command-p
+vim.keymap.set("n", "<D-p>", LazyVim.pick("files"))
+vim.keymap.set("n", "<D-S-f>", LazyVim.pick("live_grep"))
+-- Alt-Left
+vim.keymap.set("n", "<A-Left>", "b")
+vim.keymap.set("n", "<A-Right>", "w")
