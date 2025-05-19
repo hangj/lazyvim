11fc8dbdd2b6565897a201067752714618474f04
hangj <guijie.han@gmail.com>
2025-05-19 16:50:38 +0800

    change https://github.com/ to git@github.com:

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,6 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local lazyrepo = "git@github.com:folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
@@ -13,6 +13,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- change 'https://github.com/%s.git' to 'git@github.com:%s.git', to improve git clone speed
+require("lazy.core.config").defaults.git.url_format = "git@github.com:%s.git"
 
 require("lazy").setup({
   spec = {
